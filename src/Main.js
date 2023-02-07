d713a7ac8ce29bb3a5c9037db3e3f1eebeb1fc75
Fateme Ghalandari <fghalandarii@gmail.com>
2023-02-08 00:31:54 +0330

    Add css file

--- src/Main.js
+++ src/Main.js
@@ -1,14 +1,19 @@
 import { React } from 'react'
+import main_bg from "./main_bg.jpg"
 const Main = () => {
     return (
         <div className="header">
-            <div className="col1">
+            <div className="row1">
                 <h1>Here are a list of books for you to read!</h1>
             </div>
-            <div className="col2">
+            <div className="row2">
                 <h2>Book List</h2>
-                <input type="text" placeholder='Enter the book name' />
-                <button><i className="fas fa-search"></i></button>
+                <div className="search">
+                    <input type="text" placeholder='Enter the book name' />
+                    <button><i className="fas fa-search"></i></button>
+                </div>
+                
+            <img src={main_bg} alt="bg" />
             </div>
         </div>
     )
