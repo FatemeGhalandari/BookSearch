2238cadc6aad26b92e75b2987143b81bd9108614
Fateme Ghalandari <fghalandarii@gmail.com>
2023-02-12 17:47:57 +0330

    Add card component

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import { React } from 'react'
+const Card = () => {
+    return (
+        <>
+            <div className="card">
+                <img src="./b2.jpg" alt="" />
+                <div className="bottom">
+                    <h3 className="title">
+                        In Search of Lost Time
+                    </h3>
+                    <p className="amount">$18.87</p>
+                </div>
+            </div>
+        </>
+    )
+}
+export default Card
--- src/Main.js
+++ src/Main.js
@@ -1,21 +1,24 @@
 import { React } from 'react'
-import main_bg from "./main_bg.jpg"
+import Card from './Card'
 const Main = () => {
     return (
-        <div className="header">
-            <div className="row1">
-                <h1>Here are a list of books for you to read!</h1>
-            </div>
-            <div className="row2">
-                <h2>Book List</h2>
-                <div className="search">
-                    <input type="text" placeholder='Enter the book name' />
-                    <button><i className="fas fa-search"></i></button>
+        <>
+            <div className="header">
+                <div className="row1">
+                    <h1>Here are a list of books<br/> for you to read!</h1>
                 </div>
-                
-            <img src={main_bg} alt="bg" />
+                <div className="row2">
+                    <h2>Book List</h2>
+                    <div className="search">
+                        <input type="text" placeholder='Enter the book name' />
+                        <button><i className="fas fa-search"></i></button>
+                    </div>
+                </div>
             </div>
-        </div>
+            <div className="container">
+                <Card />
+            </div>
+        </>
     )
 }
 export default Main
