45f9b069f1ac3d656a1780b21f68687f7cbec743
Fateme Ghalandari <fghalandarii@gmail.com>
2023-02-17 23:03:26 +0330

    Fix bugs

--- src/Card.js
+++ src/Card.js
@@ -1,20 +1,25 @@
 import { React } from 'react'
-import b1 from './b2.jpg'
-const Card = (bookData) => {
-    return (
-        <>
-            {bookData.items.map((item) => {
+const Card = ({ bookData }) => {
+    console.log(bookData)
+    return(
+        bookData.map((item) => {
+            let book_img = item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail
+            let book_title = item.volumeInfo.title
+            let book_price = item.saleInfo.listPrice && item.saleInfo.listPrice.amount
+            if (book_img != undefined && book_price!= undefined) {
                 return (
-                    <div className="card">
-                        <img src={b1} alt="" />
-                        <div className="bottom">
-                            <h3 className="title">In Search of Lost Time</h3>
-                            <p className="amount">$18.87</p>
+                    <>
+                        <div className="card" key={item.id}>
+                            <img src={book_img} alt="" />
+                            <div className="bottom">
+                                <h3 className="title">{book_title}</h3>
+                                <p className="amount">$ {book_price}</p>
+                            </div>
                         </div>
-                    </div>
+                    </>
                 )
-            })}
-        </>
+            }
+        })
     )
 }
 export default Card
