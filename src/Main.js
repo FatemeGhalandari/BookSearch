e1b9984cc2fcbcfd36b202ccd4006cddf625e131
Fateme Ghalandari <fghalandarii@gmail.com>
2023-02-17 23:03:26 +0330

    Fix bugs

--- src/Main.js
+++ src/Main.js
@@ -6,7 +6,10 @@ const Main = () => {
     const [bookData, setBookData] = useState([]);
     const searchBook = (e) => {
         if (e.key === "Enter"){
-            axios.get('https://www.googleapis.com/books/v1/volumes?q='+search+'&key=AIzaSyDumNH8oyTQXjmCmfjcLn1II64OHJdXfkU')
+            axios.get('https://www.googleapis.com/books/v1/volumes?q='
+                + search +
+                '&key=AIzaSyDumNH8oyTQXjmCmfjcLn1II64OHJdXfkU' +
+                '&maxResults=40')
             .then(res => console.log(setBookData(res.data.items)))
             .catch(err => console.log(err))
         }
@@ -15,7 +18,7 @@ const Main = () => {
         <>
             <div className="header">
                 <div className="row1">
-                    <h1>Here are a list of books<br/> for you to read!</h1>
+                    <h1>Here are a list of books for you to read!</h1>
                 </div>
                 <div className="row2">
                     <h2>Book List</h2>
@@ -28,7 +31,9 @@ const Main = () => {
                             }
                             onKeyDown={searchBook}
                         />
-                        <button><i className="fas fa-search"></i></button>
+                        <button >
+                            <i className="fas fa-search">                            </i>
+                        </button>
                     </div>
                 </div>
             </div>
