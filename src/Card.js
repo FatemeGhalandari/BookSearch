4ea1385df54714e67b73aa1f35eb9a358e829a98
Fateme Ghalandari <fghalandarii@gmail.com>
2023-03-07 14:01:39 +0330

    fix cards and pop-up

--- src/Card.js
+++ src/Card.js
@@ -1,26 +1,34 @@
 import { React } from "react";
+import { useState } from "react";
+import PopUp from "./PopUp";
 const Card = ({ bookData }) => {
   console.log(bookData);
+  const [show, setShow] = useState(false);
+  const [bookItem, setItem] = useState();
   return bookData.map((item) => {
     let book_img =
       item.volumeInfo.imageLinks && item.volumeInfo.imageLinks.smallThumbnail;
     let book_title = item.volumeInfo.title;
     let book_price = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
-    if (book_img !== undefined && book_price !== undefined) {
+    if (book_img != undefined && book_price != undefined) {
       return (
         <>
-          <div className="card" key={item.id}>
+          <div
+            className="card"
+            onClick={() => {
+              setShow(true);
+              setItem(item);
+            }}
+          >
             <img src={book_img} alt="" />
             <div className="bottom">
               <h3 className="title">{book_title}</h3>
               <p className="amount">$ {book_price}</p>
             </div>
           </div>
+          <PopUp show={show} item={bookItem} onClose={() => setShow(false)} />
         </>
       );
-    } else {
-      console.log("book image or price is undefined");
-      return null;
     }
   });
 };
--- src/Main.js
+++ src/Main.js
@@ -1,46 +1,52 @@
-import { React, useState } from 'react'
-import Card from './Card'
-import axios from 'axios'
+import { React, useState } from "react";
+import Card from "./Card";
+import axios from "axios";
+import { Button } from "@mui/material";
 const Main = () => {
-    const [search, setSearch] = useState("");
-    const [bookData, setBookData] = useState([]);
-    const searchBook = (e) => {
-        if (e.key === "Enter"){
-            axios.get('https://www.googleapis.com/books/v1/volumes?q='
-                + search +
-                '&key=AIzaSyDumNH8oyTQXjmCmfjcLn1II64OHJdXfkU' +
-                '&maxResults=40')
-            .then(res => console.log(setBookData(res.data.items)))
-            .catch(err => console.log(err))
-        }
+  const [search, setSearch] = useState("");
+  const [bookData, setBookData] = useState([]);
+  const searchBook = (e) => {
+    if (e.key === "Enter") {
+      axios
+        .get(
+          "https://www.googleapis.com/books/v1/volumes?q=" +
+            search +
+            "&key=AIzaSyDumNH8oyTQXjmCmfjcLn1II64OHJdXfkU" +
+            "&maxResults=40"
+        )
+        .then((res) => console.log(setBookData(res.data.items)))
+        .catch((err) => console.log(err));
     }
-    return (
-        <>
-            <div className="header">
-                <div className="row1">
-                    <h1>Here are a list of books for you to read!</h1>
-                </div>
-                <div className="row2">
-                    <h2>Book List</h2>
-                    <div className="search">
-                        <input type="text"
-                            placeholder='Enter the book name' 
-                            value={search}
-                            onChange={
-                                e => {setSearch(e.target.value)}
-                            }
-                            onKeyDown={searchBook}
-                        />
-                        <button >
-                            <i className="fas fa-search">                            </i>
-                        </button>
-                    </div>
-                </div>
-            </div>
-            <div className="container">
-                <Card bookData={bookData } />
-            </div>
-        </>
-    )
-}
-export default Main
+  };
+  return (
+    <>
+      <div className="header">
+        <div className="row1">
+          <h1>Here are a list of books for you to read!</h1>
+          <Button variant="contained">Hello World</Button>
+        </div>
+        <div className="row2">
+          <h2>Book List</h2>
+          <div className="search">
+            <input
+              type="text"
+              placeholder="Enter the book name"
+              value={search}
+              onChange={(e) => {
+                setSearch(e.target.value);
+              }}
+              onKeyDown={searchBook}
+            />
+            <button>
+              <i className="fas fa-search"> </i>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <Card bookData={bookData} />
+      </div>
+    </>
+  );
+};
+export default Main;
